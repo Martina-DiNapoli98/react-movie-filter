8c17e2653088e4b2c73d3d5255d1a610cb5bc7df
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-03-27 17:21:57 +0100

    Bonus: Add search functionality to filter movies by title

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,21 @@ import movies from './data/movies'
 function App() {
   const [newMovies, setNewMovies] = useState(movies)
   const [selectedOption, setSelectedOption] = useState('');
+  const [searchQuery, setSearchQuery] = useState('')
   const options = ['Fantascienza', 'Thriller', 'Romantico', 'Azione'];
 
 
   const handleChange = (e) => {
     setSelectedOption(e.target.value);
+  };
+  const handleSearchQuery = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+
+    setSearchQuery('')
   };
 
   useEffect(() => {
@@ -18,12 +28,29 @@ function App() {
   }, [selectedOption])
 
 
+  useEffect(() => {
+    setNewMovies(movies.filter(movie => movie.title.toLowerCase().includes(searchQuery.toLowerCase()))
+    );
+  }, [searchQuery])
+
   return (
     <>
       <div className='container'>
         <h1>
           My Movies
         </h1>
+        <form onSubmit={handleFormSubmit}>
+          <input type="text"
+            className="search"
+            name="search"
+            id="search"
+            placeholder='type here your movie'
+            value={searchQuery}
+            onChange={handleSearchQuery}
+          />
+          <button className='btn btn-primary' type='submit'>submit</button>
+        </form>
+
         <label htmlFor="options" className="form-label">Scegli un'opzione:</label>
         <select
           id="options"
