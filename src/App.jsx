4f4b5b5fea3a8b2dbcfcba9212086453ad0be5ef
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-03-27 15:12:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import movies from './data/movies'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
